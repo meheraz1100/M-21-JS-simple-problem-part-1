02f3443de3149874b2b4b9465fef4a3910b5114f
meheraz1100 <meheraz1100@gmail.com>
2024-02-13 10:34:59 +0600

    important math function in js

---
+++ math.js
@@ -0,0 +1,29 @@
+// const Math = {
+//     min : function min(num1, num2){
+//         return num2;
+//     },
+//     max : function max(num1, num2){
+//         return num1;
+//     }
+// }
+
+// console.log(Math.min);
+// console.log(Math.max);
+
+// const min = Math.min
+
+
+
+const min = Math.min(45, 12, 5, 56, 99, 21, -99);
+console.log("Min Number is : ", min);
+
+const max = Math.max(45, 12, 5, 56, 99, 21, -99);
+console.log("Max number is : ", max);
+
+console.log("Value of PI : ", Math.PI);
+console.log("The Absolute Value of this Number : ",  Math.abs(-242));
+console.log("The rounded Numbers is : ", Math.round(324.234));
+console.log("This number is increased : ",  Math.ceil(1.9));
+console.log("This number is decreased : ",  Math.floor(1.89));
+let random = ("You can get a unique number : ", Math.random() * 10);
+console.log(Math.round(random));
